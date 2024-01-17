ffb04ff28fb4adc26196d98b69c7f28abae02289
Lim Media Dev2 <dev2@limmedia.io>
2024-01-16 21:32:15 -0600

    removing folder

--- NoBrandPetWasteProducts-Pinnacle-1.2.7/assets/js/polyfills.js
+++
@@ -1,8 +0,0 @@
-import 'core-js/stable';
-import 'regenerator-runtime/runtime';
-import 'whatwg-fetch';
-import objectFitImages from 'object-fit-images';
-
-document.addEventListener('DOMContentLoaded', () => {
-    objectFitImages();
-});
